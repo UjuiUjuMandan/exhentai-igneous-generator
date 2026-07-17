a1c4c24a44931565d102ec83eaa042f0139d2643
ᡠᠵᡠᡳ ᡠᠵᡠ ᠮᠠᠨᡩ᠋ᠠᠨ <125150101+UjuiUjuMandan@users.noreply.github.com>
2026-07-17 17:41:18 +0800

    Update browsing country extract regex

--- api/index.js
+++ api/index.js
@@ -53,7 +53,7 @@ export async function index(context, req) {
     const uconfigUrl = "https://e-hentai.org/uconfig.php";
     const uconfigResponse = await fetch(uconfigUrl, { headers });
     const html = await uconfigResponse.text();
-    const match = html.match(/<p>You appear to be browsing the site from <strong>(.*?)<\/strong>/);
+    const match = html.match(/<p>You appear to be located in <strong>(.*?)<\/strong>/);
     const browsingCountry = match ? match[1] : "Unknown";
 
     context.res.body = {
